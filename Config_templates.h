551f9f294a4ce78832717691a8a997e84814d147
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-07-23 15:42:14 +0200

    file to define templates for class "Config"

---
+++ Config_templates.h
@@ -0,0 +1,23 @@
+#ifndef CONFIG_TEMPLATES_H_INCLUDED
+#define CONFIG_TEMPLATES_H_INCLUDED
+
+#include <string>
+#include <sstream>
+
+template <typename T>
+bool extract(std::string& in, T& dest)
+{
+    std::stringstream sstr(in);
+    if (! (sstr >> dest))
+        return false;
+    return true;
+}
+
+template <>
+inline bool extract(std::string& in, std::string& dest)
+{
+    dest = in;
+    return true;
+}
+
+#endif // CONFIG_TEMPLATES_H_INCLUDED
