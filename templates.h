b21dba6ada6b27a99fe595b87770a862dfdf4f9f
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-07-23 15:41:40 +0200

    file to define general templates

---
+++ templates.h
@@ -0,0 +1,17 @@
+#ifndef TEMPLATES_H_INCLUDED
+#define TEMPLATES_H_INCLUDED
+
+#include "Config.h"
+
+#include <sstream>
+#include <fstream>
+
+template <typename T> std::ostream& operator<<(Config& c, T& data)
+{
+    if (!c.file.is_open())
+        return false;
+    c.file << data << c.seperator;
+    c.file.close();
+}
+
+#endif // TEMPLATES_H_INCLUDED
